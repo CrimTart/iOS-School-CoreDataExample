a0e2a11fce757cd2593ac7cfcef3e2d3044325cd
user <iOS-School-1@iMac-user-4.local>
2017-05-30 19:32:45 +0300

    Initial Commit

---
+++ CoreDataDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreDataDemo
+//
+//  Created by iOS-School-1 on 30.05.17.
+//  Copyright © 2017 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
